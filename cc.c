582f8b6a766cb93d82f3f25d6bd64e54c345a75c
DaisukeSato56 <workshift56@gmail.com>
2020-09-15 22:44:51 +0900

    add multiple functions for paesing

--- cc.c
+++ cc.c
@@ -30,23 +30,6 @@ typedef enum {
   TK_EOF,      // 入力の終わりを表すトークン
 } TokenKind;
 
-Node *new_node(Node kind, Node *lhs, Node *rhs)
-{
-  Node *node = calloc(1, sizeof(Node));
-  node->kind = kind;
-  node->lhs = lhs;
-  node->rhs = rhs;
-  return node;
-}
-
-Node *new_node_num(int val)
-{
-  Node *node = calloc(1, sizeof(Node));
-  node->kind = ND_NUM;
-  node->val = val;
-  return node;
-}
-
 typedef struct Token Token;
 
 struct Token
@@ -155,6 +138,64 @@ Token *tokenize()
 
   new_token(TK_EOF, cur, p);
   return head.next;
+}
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs)
+{
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_num(int val)
+{
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+Node *expr()
+{
+  Node *node = mul();
+
+  for (;;)
+  {
+    if (consume('+'))
+      node = new_node(ND_ADD, node, mul());
+    else if (consume('-'))
+      node = new_node(ND_SUB, node, mul());
+    else
+      return node;
+  }
+}
+
+Node *mul()
+{
+  Node *node = primary();
+
+  for (;;)
+  {
+    if (consume('*'))
+      node = new_node(ND_MUL, node, primary());
+    else if (consume('-'))
+      node = new_node(ND_DIV, node, primary());
+    else
+      return node;
+  }
+}
+
+Node *primary()
+{
+  if (consume('('))
+  {
+    Node *node = expr();
+    expect(')');
+    return node;
+  }
+  return new_node_num(expect_number())
 }
 
 int main(int argc, char **argv)
